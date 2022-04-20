fae36c826b89f8ceff795144a1f0e90e1d09fd69
frozenbanana <henrybergstrom@protonmail.com>
2022-04-20 08:49:59 +0200

    quote of the day

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+three simple examples of CLI-applications
+1. Logger
+2. Quote of the day
+3. Todo app
--- app.js
+++ app.js
@@ -2,5 +2,19 @@
 
 import chalk from 'chalk';
 
-console.log('Look mum, I am logging');
-console.log(chalk.green('Look mum, I even got colors!'));
+const url = "https://quotes.rest/qod";
+
+fetch(url, {
+  method: 'get',
+  headers: { 'Accept': 'application/json' }, // this api needs this header set for the request
+})
+.then(res => res.json())
+.then(data => {
+  const quote = data.contents.quotes[0].quote
+  const author = data.contents.quotes[0].author
+  const log = chalk.green(`${quote} - ${author}`) // we use chalk to set the color green on successful response
+  console.log(log);
+}).catch(err => {
+  const log = chalk.red(err) // we set the color red here for errors.
+  console.log(log);
+})
