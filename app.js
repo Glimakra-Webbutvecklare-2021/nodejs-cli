76e0d428b395a5dd70596573b83bc3720dce0b92
frozenbanana <henrybergstrom@protonmail.com>
2022-04-28 12:38:04 +0200

    more comments

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import { todoController } from "./controllers/main.js";
 import { todoViews } from "./views/main.js";
 
+// Varibles that captures the intent of the user
 const currentCommand = process.argv[2];
 const currentTodoNumber = process.argv[3];
+
+// Our list of supported commands
 const supportedCommands = ['new', 'get', 'complete', 'clear', 'help'];
 
-// check if currentCommand exists AND is not included in Supported Commands Array
+// check if currentCommand exists AND is not included in `supportedCommands`
 if (currentCommand && !supportedCommands.includes(currentCommand)) {
     console.log(todoViews.commandNotSupportedError);
     process.exit(1);
-} 
+}
 
+// TODO: make sure if running `complete` currentTodoNumber is defined!
+
+// Router: Redirects to specific controller method based on currentCommand
 switch (currentCommand) {
     case 'new':
         todoController.createTodo();
